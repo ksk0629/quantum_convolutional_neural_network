61dc8a67afa2aaefd87ae063a989672e8d6a8e51
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-09-29 07:40:18 +0100

    #1 Add tests Note that this commit is based on the test driven approach, which means those cases have not been passed yet.

--- tests/test_quant_conv_layer.py
+++ tests/test_quant_conv_layer.py
@@ -16,3 +16,70 @@ class TestQuantConvLayer:
         )
         assert cls.quant_conv_layer.num_qubits == cls.num_qubits
         assert cls.quant_conv_layer.param_prefix == cls.param_prefix
+
+    @pytest.mark.parametrize("num_qubits", [1, 2, 10])
+    @pytest.mark.parametrize("param_prefix", ["", "param_prefix", "t e s t"])
+    def test_init(self, num_qubits, param_prefix):
+        """Normal test;
+        Initialises the QuantConvLayer with normal arguments.
+
+        Check if
+        - the returned value has num_qubits variable begin the same as the argment.
+        - the returned value has param_prefix begin the same as the argment.
+        """
+        quant_conv_layer = QuantConvLayer(
+            num_qubits=num_qubits, param_prefix=param_prefix
+        )
+        assert quant_conv_layer.num_qubits == num_qubits
+        assert quant_conv_layer.param_prefix == param_prefix
+
+    @pytest.mark.parametrize("num_qubits", [1.1, [1], "1"])
+    def test_init_with_non_int_num_qubits(self, num_qubits):
+        """Abnormal test;
+        Initialises the QuantConvLayer with non int num_qubits.
+
+        Check if TypeError happens.
+        """
+        with pytest.raises(TypeError):
+            QuantConvLayer(num_qubits=num_qubits, param_prefix=self.param_prefix)
+
+    @pytest.mark.parametrize("num_qubits", [0, -1])
+    def test_init_with_negative_num_qubits(self, num_qubits):
+        """Abnormal test;
+        Initialises the QuantConvLayer with non positive num_qubits.
+
+        Check if ValueError happens.
+        """
+        with pytest.raises(ValueError):
+            QuantConvLayer(num_qubits=num_qubits, param_prefix=self.param_prefix)
+
+    @pytest.mark.parametrize("param_prefix", [1, 1.1, ["t e s t"]])
+    def test_init_with_wrong_non_str_param_prefix(self, param_prefix):
+        """Abnormal test;
+        Initialises the QuantConvLayer with non str param_prefix.
+
+        Check if TypeError happens.
+        """
+        with pytest.raises(TypeError):
+            QuantConvLayer(num_qubits=self.num_qubits, param_prefix=param_prefix)
+
+    @pytest.mark.parametrize("num_qubits", [1, 2, 10])
+    def test_get_circuit(self, num_qubits):
+        """Normal test;
+        Run the get_circuit function.
+
+        Check if
+        - the return value is qiskit.QuantumCircuit.
+        - the returned circuit has the parameters attribute.
+        - the length of the returned circuit's parameters attribute is 3 * num_qubits
+            if num_qubits is greater than 1. Otherwise 1.
+        """
+        quant_conv_layer = QuantConvLayer(
+            num_qubits=num_qubits, param_prefix=self.param_prefix
+        )
+        conv_layer_circuit = quant_conv_layer.get_circuit()
+
+        assert isinstance(conv_layer_circuit, qiskit.QuantumCircuit)
+        params = conv_layer_circuit.parameters
+        num_params = num_qubits * 3 if num_qubits > 1 else 1
+        assert len(params) == num_params
