b455f799d7204b65698cc421b172562eebb307c0
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-09-29 07:38:57 +0100

    #1 Add setup function

--- tests/test_quant_conv_layer.py
+++ tests/test_quant_conv_layer.py
@@ -1,8 +1,18 @@
 import pytest
+import qiskit
 
 from src.quant_conv_layer import QuantConvLayer
 
 
 class TestQuantConvLayer:
-    def test_init(self):
-        quant_conv_layer = QuantConvLayer()
+
+    @classmethod
+    def setup_class(cls):
+        """Create and set the QuantConvLayer class as a member variable for later uses."""
+        cls.num_qubits = 4
+        cls.param_prefix = "theta"
+        cls.quant_conv_layer = QuantConvLayer(
+            num_qubits=cls.num_qubits, param_prefix=cls.param_prefix
+        )
+        assert cls.quant_conv_layer.num_qubits == cls.num_qubits
+        assert cls.quant_conv_layer.param_prefix == cls.param_prefix
