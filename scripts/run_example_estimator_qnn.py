180d85fc39135e6b392da0b81ff272cca1bdf459
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-10-04 03:17:41 +0100

    #1 Add comments

--- scripts/run_example_estimator_qnn.py
+++ scripts/run_example_estimator_qnn.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-    pass
+    # Get the qiskit example QNN.
+    
+    # Set the classifier.
+    
+    # Get the dataset.
+    
+    # Get the training and testing datasets.
+    
+    # Fit the model.
+
+    # Test the model.
+    
+    # Save the model.
+
+    # Load the saved model as the test.
+    
+    # Test the saved model as the test.
