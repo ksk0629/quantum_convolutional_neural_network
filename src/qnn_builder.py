71e342551a4aa8cc5413654e6ff47dcb48c0e85c
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-10-17 13:27:08 +0100

    #5 Not give data_size when initialised but getting QNN every time

--- src/qnn_builder.py
+++ src/qnn_builder.py
@@ -8,12 +8,8 @@ from src.quant_pool_layer import QuantPoolLayer
 class QNNBuilder:
     """QNN builder class"""
 
-    def __init__(self, data_size: int):
-        """Initialise this builder.
-
-        :param int data_size: data size
-        """
-        self.data_size = data_size
+    def __init__(self):
+        """Initialise this builder."""
 
     def get_example_estimator_qnn(self) -> EstimatorQNN:
         """Get the EstimatorQNN introduced in the qiskit example.
@@ -22,22 +18,24 @@ class QNNBuilder:
         """
         return self.get_example_structure_etimator_qcnn(8)
 
-    def get_example_structure_etimator_qnn(self) -> EstimatorQNN:
+    def get_example_structure_etimator_qnn(self, data_size: int) -> EstimatorQNN:
         """Get the QCNN having the structure as follows.
         First, there is the ZFeatureMap,
         and then there are series of the ordered sets of the QuantConvLayer and QuantPoolLayer
         until the number of active qubits is one.
+
+        :param int data_size: data size
         """
         feature_map = self.__get_z_feature_map()
         ansatz = self.__get_ansatz()
 
         # Combine the feature map and ansatz.
-        circuit = qiskit.QuantumCircuit(self.data_size)
-        circuit.compose(feature_map, range(self.data_size), inplace=True)
-        circuit.compose(ansatz, range(self.data_size), inplace=True)
+        circuit = qiskit.QuantumCircuit(data_size)
+        circuit.compose(feature_map, range(data_size), inplace=True)
+        circuit.compose(ansatz, range(data_size), inplace=True)
 
         observable = qiskit.quantum_info.SparsePauliOp.from_list(
-            [("Z" + "I" * (self.data_size - 1), 1)]
+            [("Z" + "I" * (data_size - 1), 1)]
         )
 
         return EstimatorQNN(
@@ -47,33 +45,35 @@ class QNNBuilder:
             weight_params=ansatz.parameters,
         )
 
-    def __get_z_feature_map(self) -> qiskit.QuantumCircuit:
+    def __get_z_feature_map(self, data_size: int) -> qiskit.QuantumCircuit:
         """Get the quantum circuit representing ZFeatureMap.
 
+        :param int data_size: data size
         :return qiskit.QuantumCircuit: quantum circuit representing ZFeatureMap
         """
         # Create the feature map.
-        feature_map = qiskit.circuit.library.ZFeatureMap(self.data_size)
+        feature_map = qiskit.circuit.library.ZFeatureMap(data_size)
         feature_map.barrier()
 
         return feature_map
 
-    def __get_ansatz(self) -> qiskit.QuantumCircuit:
+    def __get_ansatz(self, data_size: int) -> qiskit.QuantumCircuit:
         """Get the quantum circuit having the following structure;
         There are series of the ordered sets of the QuantConvLayer and QuantPoolLayer
         until the number of active qubits is one.
 
+        :param int data_size: data size
         :return qiskit.QuantumCircuit: series of QuantConvLayer and QuantPoolLayer
         """
         # Create the ansatz.
-        current_data_size = self.data_size
+        current_data_size = data_size
         ansatz = qiskit.QuantumCircuit(current_data_size, name="Ansatz")
         index = 1
         while current_data_size != 1:
             # Create the quantum convolutional layer.
             ansatz.compose(
                 QuantConvLayer(current_data_size, f"conv{index}").get_circuit(),
-                list(range(self.data_size - current_data_size, self.data_size)),
+                list(range(data_size - current_data_size, data_size)),
                 inplace=True,
             )
             # Create the pooling layer.
@@ -81,7 +81,7 @@ class QNNBuilder:
             sinks = range(current_data_size // 2, current_data_size)
             ansatz.compose(
                 QuantPoolLayer(sources, sinks, f"pool{index}").get_circuit(),
-                list(range(self.data_size - current_data_size, self.data_size)),
+                list(range(data_size - current_data_size, data_size)),
                 inplace=True,
             )
             # Reduce the data size.
