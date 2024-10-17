3697cc052be6c91794ef0b862e43bff0fd980c2d
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-10-17 13:34:17 +0100

    #5 Create the test class of QNNBuilder

---
+++ tests/test_qnn_builder.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.qnn_builder import QNNBuilder
+
+
+class TestQNNBuilder:
+
+    @classmethod
+    def setup_class(cls):
+        """Create and set the QNNuilder class as a member variable for later uses."""
+        cls.qnn_builder = QNNBuilder()
+
+    def test_get_example_estimator_qnn(self):
+        """Normal test;
+        Runs get_example_estimator_qnn.
+
+        Check if no error happens.
+        """
+        self.qnn_builder.get_example_estimator_qnn()
+
+    @pytest.mark.parametrize("data_size", [2, 4, 16])
+    def test_get_example_structure_estimator_qnn(self, data_size):
+        """Normal test;
+        Runs get_example_structure_estimator_qnn with valid data_size.
+
+        Check if no error happens.
+        """
+        self.qnn_builder.get_example_structure_estimator_qnn(data_size)
