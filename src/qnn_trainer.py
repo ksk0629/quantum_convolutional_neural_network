ef37b190d3a71e874dadc92f3b6b3e4aa70a3552
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-10-23 15:01:31 +0100

    #13 Add the docstring of __init__

--- src/qnn_trainer.py
+++ src/qnn_trainer.py
@@ -26,6 +26,18 @@ class QNNTrainer:
         callback: None | Callable[[np.ndarray, float], None] | None = None,
         seed: None | int = 91,
     ):
+        """Iniitalise this class.
+
+        :param qiskit.primitives.BaseEstimator | qiskit.primitives.BaseSampler qnn: QNN
+        :param None  |  qiskit_algorithms.optimizers.Optimizer  |  qiskit_algorithms.optimizers.Minimizer optimiser: optimiser
+        :param np.typing.ArrayLike train_data: train data
+        :param np.typing.ArrayLike train_labels: train label
+        :param np.typing.ArrayLike test_data: test data
+        :param np.typing.ArrayLike test_labels: test label
+        :param None | np.ndarray initial_point: initial point, defaults to None
+        :param None | Callable[[np.ndarray, float], None] | None callback: callback, defaults to None
+        :param None | int seed: random seed, defaults to 91
+        """
         self.qnn = qnn
         self.optimiser = optimiser
         self.initial_point = initial_point
