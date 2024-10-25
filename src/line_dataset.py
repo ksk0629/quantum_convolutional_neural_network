18e268389ee2040321cdaa308b7548e683a49de1
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-10-25 14:45:46 +0100

    #17 Create initial LineDataset class

---
+++ src/line_dataset.py
@@ -0,0 +1,8 @@
+class LineDataset:
+    """Line dataset class.
+    This class is generalisation of the dataset introduced in the qiskit tutorial.
+    See https://qiskit-community.github.io/qiskit-machine-learning/tutorials/11_quantum_convolutional_neural_networks.html.
+    """
+
+    def __init__(self):
+        pass
